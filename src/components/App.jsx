44ed9159d7967b0559ba6d8357ccbaaa36705f31
JohanDevia <johandeviag@gmail.com>
2024-05-01 21:22:54 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,10 @@ const Movies = lazy(() => import('../components/Movies/Movies'));
 const MovieDetails = lazy(() =>
   import('../components/MovieDetails/MovieDetails')
 );
+const Cast = lazy(() => import('../components/MovieDetails/Cast/Cast'));
+const Reviews = lazy(() =>
+  import('../components/MovieDetails/Reviews/Reviews')
+);
 
 function App() {
   return (
@@ -22,6 +26,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
